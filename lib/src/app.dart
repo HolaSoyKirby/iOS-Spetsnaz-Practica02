ef9d7631542378b34d3aaa87c5abed8c0ddffd3e
Tweek-Tweak <61298009+Tweek-Tweak@users.noreply.github.com>
2021-03-05 20:18:08 -0600

    Kasjsjsjks

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:pr03/src/pages/home_page.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  build(context) {
+    return MaterialApp(
+        home: Center(
+      child: CountPage(),
+    ));
+  }
+}
